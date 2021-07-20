0c4168958142dca12a924565274530d8b6184741
Jennifer Fix <jenniferashleyfix@gmail.com>
2021-07-19 22:36:11 -0600

    remove import for css file (duh!)

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 
 import { GlobalStyle } from "./GlobalStyle";
 
-import "./assets/clock.css";
-
 import Clock from "./components/Clock/Clock";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
